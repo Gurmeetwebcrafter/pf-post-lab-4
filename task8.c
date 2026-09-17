06dc02127b1b8ee8892919c43b06f64b637d852b
Gurmeetwebcrafter <145454904+Gurmeetwebcrafter@users.noreply.github.com>
2026-09-18 02:44:01 +0500

    Update task8.c

---
+++ task8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter marks : ");
+    scanf("%d", &marks);
+    if (marks >=50)
+    {
+        printf("Pass");
+
+    }
+    else {
+        printf("Fail");
+    }
+}
